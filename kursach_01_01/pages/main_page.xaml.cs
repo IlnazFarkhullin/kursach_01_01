bea1994fb6361fd2936dabba20294a62c2753891
IlnazFarkhullin <90451035+IlnazFarkhullin@users.noreply.github.com>
2022-04-13 12:47:31 +0300

    13/04/2022/1

---
+++ kursach_01_01/pages/main_page.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace kursach_01_01.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для main_page.xaml
+    /// </summary>
+    /// 
+   
+
+    public partial class main_page : Page
+    {
+
+        DispatcherTimer timer;
+        double panelWidth;
+        bool hidden;
+ 
+        public main_page()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,0,0,10);
+            timer.Tick += Timer_Tick;
+            panelWidth = sidePanel.Width;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                sidePanel.Width += 1;
+                if (sidePanel.Width >= panelWidth)
+                {
+                    timer.Stop();
+                    hidden = false;
+                }
+            }
+            else
+            {
+                sidePanel.Width -= 1;
+                if (sidePanel.Width <= 35)
+                {
+                    timer.Stop();
+                    hidden = true;
+                }
+            }
+
+        }
+
+        private void menu_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void pahel_header_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void DragMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
